e5a85057d4815984d3dbf77791c773694d293164
David Lin <dave.lin21@gmail.com>
2017-02-28 01:56:56 -0500

    created search bar component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyAnjj4QDfxeYPmX4bVlYMeE8StNATEz2xQ'
+
 // Create a new component. This component should produce some HTML
 
-const App = function() {
-// const is ES6 syntax; declaring a variable but cannot change the variable; a constant variable
-  return <ol>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ol>
+const App = () => {
+  return <div>Hi!</div>
 }
-
+// const is ES6 syntax; declaring a variable but cannot change the variable; a constant variable
 
 // Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
