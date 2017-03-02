75481f34d46ea05c3f5fddc0189272bfc9afbc9b
David Lin <dave.lin21@gmail.com>
2017-03-02 01:23:58 -0500

    created video list items

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyAnjj4QDfxeYPmX4bVlYMeE8StNATEz2xQ';
 
-const API_KEY = 'AIzaSyAnjj4QDfxeYPmX4bVlYMeE8StNATEz2xQ'
 
 // Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos });
+      //can also use this.setState({ videos});
+    });
+  }
+
+  render() {
+    return(
+      <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 // const is ES6 syntax; declaring a variable but cannot change the variable; a constant variable
 
